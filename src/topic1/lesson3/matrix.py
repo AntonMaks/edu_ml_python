0512bf8a4cb36d5abff21159b21764f3888b825e
Anton Maksymchuk <antonmaksymchuk1@gmail.com>
2021-03-23 20:19:04 +0200

    home work matrix

--- src/topic1/lesson3/matrix.py
+++ src/topic1/lesson3/matrix.py
@@ -1,25 +1,34 @@
 if __name__ == '__main__':
-
     M1 = [[1, 2, 3, 4, 11],
           [5, -6, 7, 8, 11],
           [5, 2, -42, 8, 11],
           [1, 6, 7, 11, 100]]
 
-    # TODO print ALL elements of matrix
+    # print ALL elements of matrix
     for i in range(len(M1)):
-        for j in range(len(M1)):
+        for j in range(len(M1[i])):
             print(M1[i][j])
+    print("==============")
 
     M2 = [[1, 2, 3, 4],
           [5, -6, 7, 8],
           [5, 6, 5, 8],
           [1, 6, 7, 11]]
-    # TODO print diagonal - 1,-6,5,11
-    print(M2)
+
+    # print diagonal - 1,-6,5,11
+    index = 0
+    for i in range(len(M2[index])):
+        print(M2[i][index])
+        index += 1
+    print("==============")
 
     M3 = [[1, 2, 3, 4],
           [5, 2, 7, 8],
           [5, 6, 3, 8],
           [1, 6, 7, 4]]
-    # TODO print diagonal in oposite order  4, 3, 2, 1
-    print(M3)
+
+    # print diagonal in opposite order  4, 3, 2, 1
+    index_M3 = (len(M3) - 1)
+    while index_M3 >= 0:
+        print(M3[index_M3][index_M3])
+        index_M3 -= 1
